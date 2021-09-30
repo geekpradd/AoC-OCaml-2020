4a7ad51f2a5a2eba7f4dbe19d9e4a82e3a3e8cf9
Pradipta Bora <190050089@iitb.ac.in>
2021-09-30 14:52:16 +0530

    add day2

---
+++ day_02/day_02.ml
@@ -0,0 +1,41 @@
+open Base
+open Stdio
+
+let explode s = String.to_list s
+
+let parse_count str = 
+  match String.split str ~on:'-' with 
+  | first::second :: _ ->  (Int.of_string first, Int.of_string second)
+  | _ -> (-1,-1)
+
+
+let rec count str pattern = 
+  match str with
+    | first :: rest -> ( count rest pattern ) + (if (Char.equal pattern  first) then 1 else 0)
+    | [] -> 0
+
+
+let get_first str = 
+  match explode str with 
+  | first :: _ -> first
+  | _ -> '2'
+
+let valid str = 
+  match String.split str ~on:' ' with 
+  | first::second::third :: _ -> 
+      let (l, h) = parse_count first in 
+        let ctr = count (explode third) (get_first second) in 
+          if (l <= ctr && ctr <= h) then 1 else 0
+  | _ -> 0
+
+
+let rec main count  =
+  let line = In_channel.input_line In_channel.stdin in
+  match line with 
+  | None -> count
+  | Some x -> main (count + valid x)
+
+let () = 
+  let value =  main 0 in 
+      printf "Answer is %d \n" value
+
